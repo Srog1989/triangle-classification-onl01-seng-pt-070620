b35a17f23f4033d51deaa8859d7e1c7246a0bb58
Sarah Rogers <s.rogers0715@yahoo.com>
2020-08-10 01:08:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+ 
+  def initialize (side_1, side_2_, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+  
+  class TriangleError < StandardError
+  end
+  
+  def kind
+  if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
+    raise TriangleError
+    
+  elsif @side_1 == @side_2 && @side_2 == @side_3 && @side_1 == @side_3
+    :equilateral
+    
+  elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
+    :isosceles
+    
+    # elsif 
+  end
+  end
+  end
+end
+  
+ 
 end
